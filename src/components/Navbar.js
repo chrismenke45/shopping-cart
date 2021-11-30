126abc462291bc0c15166a1b6dfc1a4f6689d041
chrismenke45 <chrismenke45@gmail.com>
2021-11-29 16:20:53 -0800

    ready for deploy

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 function Navbar(props) {
     const { displayCartToggle, cartQuantity } = props
     return (
         <div id="navBar">
             {/*these are the links that lead to other oages */}
-                <h1 id="pageTitle">FORTNITE - Upcoming Items</h1>
-                <Link to="/" style={{ textDecoration: 'none' }}>
-                    <h3 className="navItem">Home</h3>
-                </Link>
-                <Link to="/shop" style={{ textDecoration: 'none' }}>
-                    <h3 className="navItem">Shop</h3>
-                </Link>
-            <button onClick={displayCartToggle}>cart<sup>{cartQuantity}</sup></button>
+            <h1 id="pageTitle">FORTNITE - Upcoming Items</h1>
+            <Link to="/" style={{ textDecoration: 'none' }}>
+                <h3 className="navItem">Home</h3>
+            </Link>
+            <Link to="/shop" style={{ textDecoration: 'none' }}>
+                <h3 className="navItem">Shop</h3>
+            </Link>
+            <button onClick={displayCartToggle}>
+                <FontAwesomeIcon icon={faShoppingCart} />
+                {cartQuantity ? //if cart is empty dont display number, otherwise display number of items
+                    <sup> {cartQuantity}</sup>
+                    :
+                    null}
+            </button>
         </div>
     );
 }
