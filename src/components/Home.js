f8ae50d1c5045965118f4a36677d45406bfefb30
chrismenke45 <chrismenke45@gmail.com>
2022-05-09 20:37:22 -0700

    not winter 2021

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
     <article className="homePageContent">
       <section className="homePageNews">
         <h2>News</h2>
-        <p>New items are coming Winter 2021! Make sure to check them out in our shop!</p>
+        <p>New items are coming! Make sure to check them out in our shop!</p>
       </section>
     </article >
   );
